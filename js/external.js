a6a668bb5d888182b28c7a97504b81707f225b19
Sebastian Angulo <sebastianangulo442@yahoo.com>
2019-08-05 16:21:28 -0500

    add java

---
+++ js/external.js
@@ -0,0 +1,15 @@
+alert('Welcome to my Website!');
+var userInput = prompt('Whats your favorite color?');
+console.log( 'The user entered: '+ userInput);
+alert("Great, " + userInput +" is my favorite color too!")
+//break//
+var littlemermaid = prompt( 'how many days was the little mermaid rented?');
+console.log( 'the user entered' + littlemermaid );
+var brotherbear = prompt('how many days was brother bear rented?');
+console.log('the user entered' + brotherbear);
+var hercules= prompt('how many days was hercules rented?');
+console.log('the user entered' + hercules);
+var dailycost= prompt( 'how much does it cost to rent dialy?');
+console.log('the user entered'+ dailycost);
+alert('total cost is '+(littlemermaid + brotherbear+ hercules)* dailycost )
+//break//
