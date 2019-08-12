0b91f8a9b597e2cfbcce22e5431d3df5a1c87631
Sebastian Angulo <sebastianangulo442@yahoo.com>
2019-08-12 15:47:57 -0500

    add while loops

--- js/while.js
+++ js/while.js
@@ -1,7 +1,21 @@
-var i = 0;
-
-while(i < 65536){
-    i = i *2;
+var i = 1;
+while (i < 65536){
+    i= i*2;
     console.log(i);
-
 }
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do{
+    var conesordered =Math.floor(Math.random() * 5) + 1;
+    if (allCones >= conesordered){
+        allCones = allCones - conesordered;
+        if (allCones === 0 ){
+            console.log(allCones, "All out of cones")
+        } else {
+            console.log(allCones , "yes i can sell you these cones");
+        }
+   } else {
+       console.log(allCones,  "sorry i cant sell you this cone")
+    }
+} while(allCones > 0);
